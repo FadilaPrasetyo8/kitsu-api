2ca02a08ca5898fe6e5b4afad0438876951a10f2
FadilaPrasetyo8 <fadilaprasetyo8@gmail.com>
2025-02-26 01:58:19 +0700

    fix env and api

--- src/components/ApiHandler.js
+++ src/components/ApiHandler.js
@@ -1,21 +1,23 @@
 import axios from "axios";
 
 export const getAnime = async () => {
-  const dataAnime = await axios.get("https://kitsu.io/api/edge/anime");
+  const dataAnime = await axios.get(`${import.meta.env.VITE_PUBLIC_BASE_URL_API}/anime`);
   return dataAnime;
 };
 
 export const getAnimeById = async (id) => {
-  const dataAnime = await axios.get(`https://kitsu.io/api/edge/anime/${id}`);
+  const dataAnime = await axios.get(`${import.meta.env.VITE_PUBLIC_BASE_URL_API}/anime/${id}`);
   return dataAnime;
 };
 
 export const trendingAnime = async () => {
-  const dataAnime = await axios.get("https://kitsu.io/api/edge/trending/anime");
+  const dataAnime = await axios.get(`${import.meta.env.VITE_PUBLIC_BASE_URL_API}/trending/anime`);
   return dataAnime;
 };
 
 export const searchAnime = async (query) => {
-  const dataAnime = await axios.get(`https://kitsu.io/api/edge/anime?filter[text]=${query}`);
+  const dataAnime = await axios.get(
+    `${import.meta.env.VITE_PUBLIC_BASE_URL_API}/anime?filter[text]=${query}`
+  );
   return dataAnime;
 };
--- src/pages/ContentPages.jsx
+++ src/pages/ContentPages.jsx
@@ -4,7 +4,7 @@ import { getAnime, searchAnime } from "../components/ApiHandler";
 import ContentTop from "../components/ContentTop";
 
 const ContentPages = () => {
-  const [animes, setAnimes] = useState([]);
+  const [animes, setAnimes] = useState({ data: [] });
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
 
@@ -26,16 +26,14 @@ const ContentPages = () => {
     if (query.length > 3) {
       try {
         const { data } = await searchAnime(query);
-        setAnimes(data.data || []);
+        setAnimes(data || []);
       } catch (error) {
         setError("Failed to search anime.");
       }
     }
   };
 
-  console.log(animes);
-
-  const bgimage = animes[0]?.attributes?.coverImage?.original;
+  const bgimage = animes?.data[0]?.attributes?.coverImage?.original;
 
   return (
     <>
